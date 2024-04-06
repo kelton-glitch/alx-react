0037d750054ef97e3ded874b4e7ee33382b7aa94
Kamgo N Elton <kamgoelton89@gmail.com>
2024-04-06 03:26:04 +0100

    task 5 complete

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -1,10 +1,30 @@
 const path = require('path');
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'production',
-  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  mode: 'development',
+  entry: "./src/index.js",
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve("dist"),
+    publicPath: "/"
   },
+  module: {
+    rules: {
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react', '@babel/preset-env']
+        }
+      }
+    }
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "index.html"
+    }),
+  ]
 };
